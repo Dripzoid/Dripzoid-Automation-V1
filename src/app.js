f7400575d9fa16d875f7b591ffcb0d399853679f
Dripzoid <dripzoidapp@gmail.com>
2026-06-09 22:35:54 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,11 +9,24 @@ import eventRoutes from "./routes/event.routes.js";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "https://api.dripzoid.com",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(morgan("dev"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use("/health", healthRoutes);
 app.use("/api/events", eventRoutes);
