976f4226d4f8e64b68eb87586e800ca39f0422b0
Dripzoid <dripzoidapp@gmail.com>
2026-06-10 12:38:34 +0530

    Create retry.scheduler.js

---
+++ src/schedulers/retry.scheduler.js
@@ -0,0 +1,106 @@
+import cron from "node-cron";
+
+import {
+  getPendingTasks,
+  updateTask,
+  createLog,
+} from "../services/scheduler.service.js";
+
+import { processEvent }
+  from "../services/eventProcessor.service.js";
+
+export function startRetryScheduler() {
+  cron.schedule("*/5 * * * *", async () => {
+    console.log(
+      "🔄 Running Retry Scheduler"
+    );
+
+    try {
+      const tasks =
+        await getPendingTasks();
+
+      for (const task of tasks) {
+        try {
+          if (
+            task.taskType !==
+            "RETRY_AUTOMATION_EVENT"
+          ) {
+            continue;
+          }
+
+          const {
+            eventType,
+            payload,
+          } = task.payload;
+
+          await processEvent(
+            eventType,
+            payload
+          );
+
+          await updateTask(
+            task.id,
+            {
+              status: "completed",
+              executedAt: new Date(),
+            }
+          );
+
+          await createLog({
+            level: "info",
+            service:
+              "retry-scheduler",
+            message:
+              "Task completed",
+            metadata: {
+              taskId: task.id,
+              eventType,
+            },
+          });
+        } catch (error) {
+          const retries =
+            task.retryCount + 1;
+
+          if (retries >= 5) {
+            await updateTask(
+              task.id,
+              {
+                status: "failed",
+                lastError:
+                  error.message,
+                retryCount:
+                  retries,
+              }
+            );
+          } else {
+            await updateTask(
+              task.id,
+              {
+                retryCount:
+                  retries,
+                lastError:
+                  error.message,
+              }
+            );
+          }
+
+          await createLog({
+            level: "error",
+            service:
+              "retry-scheduler",
+            message:
+              error.message,
+            metadata: {
+              taskId: task.id,
+            },
+          });
+        }
+      }
+    } catch (error) {
+      console.error(
+        "Scheduler Error:",
+        error.message
+      );
+    }
+  });
+}
